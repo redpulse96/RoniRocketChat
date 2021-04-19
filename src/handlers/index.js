dc915876072634464692c0f2a6632c98d29f3035
redpulse96 <impulse96@icloud.com>
2021-04-19 20:19:31 +0530

    url fix

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -1,6 +1,7 @@
 const connection = require('../connection');
 
 async function fetchQuery(req, res) {
+  console.log('----API.called----');
   const { cedule } = req.query;
   if (!cedule) {
     return res.status(400).json({ success: false, message: 'Invalid cedule' });
@@ -10,6 +11,8 @@ async function fetchQuery(req, res) {
     const dbConn = await connection();
 
     const [exec] = await dbConn.query(query1);
+    console.log('----API.executed----');
+    console.dir(exec);
     res.status(200).json({
       success: true,
       data: { ...exec[0] },
@@ -17,7 +20,7 @@ async function fetchQuery(req, res) {
   } catch (error) {
     console.log('catch.error');
     console.error(error);
-    throw error;
+    return res.status(error.statusCode || 500).json({ success: false, error });
   }
 }
 
